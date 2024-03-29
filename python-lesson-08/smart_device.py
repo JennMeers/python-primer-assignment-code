c89e10e858db668ed310e9a23c0fcdfc838275cb
JennMeers <150211193+JennMeers@users.noreply.github.com>
2024-03-29 12:37:30 +1100

    Add files via upload

---
+++ python-lesson-08/smart_device.py
@@ -0,0 +1,34 @@
+class SmartDevice:
+
+    def __init__(self, model_number, brand, screen_size, app_list=set()):
+        self.model_number = model_number
+        self.brand = brand
+        self.screen_size = screen_size
+        self.app_list = app_list
+
+    def report(self):
+        print("This is " + str(self.model_number) + " from " + self.brand)
+
+    def install_app(self, app_name="Demo App"):
+        if app_name in self.app_list:
+            print(f"{app_name} is already installed")
+        else:
+            print(f"Installing {app_name}...")
+        self.app_list.add(app_name)
+        return self.app_list
+    
+    def delete_app(self, app_name):
+        print(f"{app_name} has been deleted.")
+        self.app_list.remove(app_name)
+        return self.app_list
+
+device_a = SmartDevice(1233244, 'CLG', 5.7)
+device_a.report()
+
+device_a.install_app("Test app")
+device_a.install_app()
+device_a.install_app()
+print(device_a.app_list)
+
+device_a.delete_app("Test app")
+print(device_a.app_list)
